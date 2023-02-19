e244b46e5d5f5f6156e3913cfb987e9fb1f68d8a
A̶k̶s̶h̶a̶z̶u̶r̶ ̶|̶ ̶d̶e̶v̶.̶ <92050792+Akshazz@users.noreply.github.com>
2023-02-19 06:48:27 +0000

    Update index.php
    hostinger-update_001

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
+<?php 
+  session_start();
+  if(isset($_SESSION['unique_id'])){
+    header("location: users.php");
+  }
+?>
+<?php include_once "header.php"; ?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
         <title> MISC RTC APP </title>
+        <link rel="icon" href="images/logo.png">
         <link rel="manifest" href="manifest.json">
         <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
@@ -16,7 +25,7 @@
     <section class="form login">
       <center><img src="./images/logo.png" alt="logo" width="190px" height="190px"></center>
       <header style="color:rgb(44, 44, 44); font-size: 15; font-family:Arial, Helvetica, sans-serif;"> MISC: Sign in </header>
-      <form action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
+      <form action="php/login.php" method="POST" enctype="multipart/form-data" autocomplete="off">
         
         <div class="field input">
           <label>&nbsp Student ID</label>
