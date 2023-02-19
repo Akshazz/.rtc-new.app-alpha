eb8e5042108129425d35f30345d98d911551d57a
A̶k̶s̶h̶a̶z̶u̶r̶ ̶|̶ ̶d̶e̶v̶.̶ <92050792+Akshazz@users.noreply.github.com>
2023-02-19 07:51:10 +0000

    Update users.php

--- users.php
+++ users.php
@@ -22,9 +22,37 @@
           <center>
           <div class="details">
           <img src="php/images/<?php echo $row['img']; ?>" alt="" width="50px" height="50px" padding="10px 10px"><span><?php echo $row['unique_id']. " | " .$row['fname'] .$row['lname']?></span>
-        <a class="btn btn-primary" href="php/logout.php?logout_id=<?php echo $row['unique_id']; ?>" class="logout">Logout <i class="bx bx-log-out"></i></a>
+        
+          <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+  Sign-out
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle" style="color:red";> Do you want to sign out?</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <a class="btn btn-primary" href="php/logout.php?logout_id=<?php echo $row['unique_id']; ?>" class="logout">Logout <i class="bx bx-log-out"></i></a>
           </div>
+      </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
           </center>
+        
       </header>
 
     <!DOCTYPE html>
